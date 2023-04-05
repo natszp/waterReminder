46ecea76201123eb39fcdc0097e53804b68650b0
natszp <natalia.szpurko@gmail.com>
2023-04-05 10:23:24 +0200

    major: updated DrinkForm + css added

--- src/components/NewDrink/DrinkForm.js
+++ src/components/NewDrink/DrinkForm.js
@@ -1,26 +1,54 @@
-const DrinkForm = () => {
+import { useState } from 'react';
+import './DrinkForm.css'
 
+const DrinkForm = (props) => {
+
+    const [enteredType, setEnteredType] = useState('')
+    const[enteredAmount, setEnteredAmount] = useState('')
+
+    const typeChangeHandler = (event) => {
+        setEnteredType(event.target.value)
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+
+    const submitFormHandler = (event) => {
+        event.preventDefault();
+        const drinkData = {
+            type: enteredType,
+            amount: enteredAmount
+        }
+        props.onSaveDrinkData(drinkData);
+        setEnteredType('')
+        setEnteredAmount('')
+    }
 
     return (
         <div>
-            <form>
-                <div>
-                    <div>
+            <form onSubmit={submitFormHandler} >
+                <div className="new-drink__controls">
+                    <div className="new-drink__control">
                         <label>Type</label>
                         <input
                             type='text'
+                            value={enteredType}
+                            onChange={typeChangeHandler}
                         />
                     </div>
-                    <div>
+                    <div className='new-drink__control'>
                         <label>Amount</label>
                         <input
                             type='number'
+                            value={enteredAmount}
                             min='100'
                             step='100'
+                            onChange={amountChangeHandler}
                         />
                     </div>
                 </div>
-                <div>
+                <div className='new-drink__actions'>
                     <button type="button">Cancel</button>
                     <button type='submit'>Add Drink</button>
                 </div>
