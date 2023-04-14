0ba132d270d4d27d01a399617f7caef634f95ef7
natszp <natalia.szpurko@gmail.com>
2023-04-14 10:33:11 +0200

    minor: removing DUMMY_DRINKS & adding No drinks info

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import './App.css';
 import DrinksList from './components/Drinks/DrinksList';
 import NewDrink from './components/NewDrink/NewDrink';
 
-  const DUMMY_DRINKS = [
-    { type: 'water', amount: 200 },
-    { type: 'tea', amount: 100 },
-
-  ]
 
 const App = () => {
-  const [drinks, setDrinks] = useState(DUMMY_DRINKS)
+  const [drinks, setDrinks] = useState([])
 
 
   const addToDrinksHandler = (drink) => {
--- src/components/Drinks/DrinksList.js
+++ src/components/Drinks/DrinksList.js
@@ -4,9 +4,13 @@ import './DrinksList.css'
 
 const DrinksList = (props) => {
 
+    const noDrinksInfo = <p className="info">No drinks to show</p>
+
+
     return (
         <div>
             <Card className='drinks'>
+                {props.drinks.length == 0 && noDrinksInfo}
                 {props.drinks.map(drink =>
                     <DrinkItem
                         key={drink.id} type={drink.type} amount={drink.amount} />)}
