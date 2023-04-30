30a16774a56ac34c54d45e446e0eafe2a137470c
natszp <natalia.szpurko@gmail.com>
2023-04-30 12:07:50 +0200

    major: Check hydration Modal added

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,11 +3,14 @@ import Card from '../UI /Card';
 import './Chart.css'
 import { useState } from 'react';
 import MonitorWeightIcon from '@mui/icons-material/MonitorWeight';
+import Button from '@mui/material/Button';
+import Modal from './../UI /Modal'
 
 
 const Chart = (props) => {
 
     const [hydrationRecommended, setHydration] = useState(2000)
+    const [shownModal, setShowModal] = useState(false)
 
     let weightConverted;
 
@@ -20,13 +23,27 @@ const Chart = (props) => {
     let barFillWidth = '0%';
     barFillWidth = Math.round((props.drinksAmount / hydrationRecommended) * 100) + '%';
 
+    let actualVsRecommendedHydration = `${props.drinksAmount} / ${hydrationRecommended} ml`
+
+    const showModalHandler = () => {
+        setShowModal(true)
+        console.log(shownModal)
+    }
+
+    const hideModalHandler = () => {
+        setShowModal(false)
+    }
+
+    // to be developed 
+    const hydrationLevel = `Your hydration level is ${Math.round((props.drinksAmount/hydrationRecommended *100))}%`
+
 
     return (
         <Card>
             <div className='calculator'>
                 <WeightForm onChangeWeight={calculatorHandler} />
                 <MonitorWeightIcon color='black' fontSize='large' />
-                <div className='info'>{props.drinksAmount} / {hydrationRecommended} ml</div>
+                <div className='info' id='hydrationLevelDiv'>{actualVsRecommendedHydration}</div>
             </div>
             <div className='chart'>
                 <div className='chart-bar'>
@@ -34,12 +51,17 @@ const Chart = (props) => {
                         <div
                             className='chart-bar__fill'
                             style={{ width: barFillWidth }}
-                        ></div>
+                        >
+                        </div>
+
                     </div>
                 </div>
             </div>
+            <Button onClick={showModalHandler}>Check hydration</Button>
+            {shownModal && <Modal onOpen={shownModal} content={hydrationLevel} onConfirm={hideModalHandler}/>}
         </Card>
     )
+    
 }
 
 export default Chart;
